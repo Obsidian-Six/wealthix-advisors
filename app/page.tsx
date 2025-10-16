bb09348f30b80aee329ea57e3282aaae4c9fb443
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2025-10-16 10:21:43 +0530

    Add Core Services component and styles; enhance global styles for improved layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CoreServices from "./_components/core-services/core-services";
 import Hero from "./_components/hero/hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <CoreServices />
     </>
   );
 }

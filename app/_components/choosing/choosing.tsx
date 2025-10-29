6a373866ec77280d5c0e3e4937cd795a50ae134e
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2025-10-29 13:04:36 +0530

    feat: Update global styles and add new SVG icons
    - Modified typography styles in globals.css for better responsiveness.
    - Added new button styles for solid and outline variants.
    - Adjusted hero section styles for improved layout and spacing.
    - Updated core services card styles for better alignment and spacing.
    - Enhanced e-invoicing section styles for improved readability.
    - Added new SVG icons: advisory, arrow-right, calendar-tick, call, compliance, rapid-response, team, and whatsapp.

--- app/_components/choosing/choosing.tsx
+++ app/_components/choosing/choosing.tsx
@@ -122,34 +122,19 @@ export default async function Choosing() {
           </article>
         </li> */}
         <li>
-          <button type="button" className="cta-action">
+          <a
+            href="https://calendly.com/neetujainca/caneetujain"
+            target="_blank"
+            className="cta-action btn"
+          >
             Schedule Consultation Today
-            {/* Arrow Right */}
-            <svg
-              width="14"
-              height="20"
-              viewBox="0 0 14 20"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clipPath="url(#clip0_61_126)">
-                <path
-                  d="M3.61619 2.045C3.38185 2.27941 3.25021 2.59729 3.25021 2.92875C3.25021 3.2602 3.38185 3.57809 3.61619 3.8125L9.80369 10L3.61619 16.1875C3.38849 16.4233 3.2625 16.739 3.26535 17.0668C3.2682 17.3945 3.39966 17.708 3.63142 17.9398C3.86318 18.1715 4.17669 18.303 4.50444 18.3058C4.83218 18.3087 5.14794 18.1827 5.38369 17.955L12.4549 10.8837C12.6893 10.6493 12.8209 10.3315 12.8209 10C12.8209 9.66854 12.6893 9.35066 12.4549 9.11625L5.38369 2.045C5.14928 1.81066 4.8314 1.67902 4.49994 1.67902C4.16848 1.67902 3.8506 1.81066 3.61619 2.045Z"
-                  fill="white"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_61_126">
-                  <rect
-                    width="20"
-                    height="14"
-                    fill="white"
-                    transform="matrix(0 1 -1 0 14 0)"
-                  />
-                </clipPath>
-              </defs>
-            </svg>
-          </button>
+            <Image
+              src="/icons/arrow-right.svg"
+              alt="Arrow Right"
+              width={14}
+              height={20}
+            />
+          </a>
         </li>
       </ul>
       <figure className="choosing-figure">
--- app/_components/schedule/schedule.tsx
+++ app/_components/schedule/schedule.tsx
@@ -33,8 +33,9 @@ export default function Schedule() {
               strokeLinecap="round"
               strokeLinejoin="round"
             />
-          </svg>{" "}
-          Working Hours: <span>Mon-Fri, 8:30 AM - 5:30 PM</span>
+          </svg>
+          <span className="placeholder">Working Hours:</span>
+          <span className="time">Mon-Fri, 8:30 AM - 5:30 PM</span>
         </p>
         <p className="timing">
           <svg
@@ -48,38 +49,23 @@ export default function Schedule() {
               d="M7.5 2.5H22.5V10L17.5 15L22.5 20V27.5H7.5V20L12.5 15L7.5 10V2.5ZM20 20.625L15 15.625L10 20.625V25H20V20.625ZM15 14.375L20 9.375V5H10V9.375L15 14.375ZM12.5 7.5H17.5V8.4375L15 10.9375L12.5 8.4375V7.5Z"
               fill="#080808"
             />
-          </svg>{" "}
-          Session Duration:
-          <span>30 minutes per inquiry</span>
-        </p>
-        <button type="button" className="cta-action">
-          Book Your Free Consultation Now
-          {/* Arrow Right */}
-          <svg
-            width="14"
-            height="20"
-            viewBox="0 0 14 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g clipPath="url(#clip0_61_126)">
-              <path
-                d="M3.61619 2.045C3.38185 2.27941 3.25021 2.59729 3.25021 2.92875C3.25021 3.2602 3.38185 3.57809 3.61619 3.8125L9.80369 10L3.61619 16.1875C3.38849 16.4233 3.2625 16.739 3.26535 17.0668C3.2682 17.3945 3.39966 17.708 3.63142 17.9398C3.86318 18.1715 4.17669 18.303 4.50444 18.3058C4.83218 18.3087 5.14794 18.1827 5.38369 17.955L12.4549 10.8837C12.6893 10.6493 12.8209 10.3315 12.8209 10C12.8209 9.66854 12.6893 9.35066 12.4549 9.11625L5.38369 2.045C5.14928 1.81066 4.8314 1.67902 4.49994 1.67902C4.16848 1.67902 3.8506 1.81066 3.61619 2.045Z"
-                fill="white"
-              />
-            </g>
-            <defs>
-              <clipPath id="clip0_61_126">
-                <rect
-                  width="20"
-                  height="14"
-                  fill="white"
-                  transform="matrix(0 1 -1 0 14 0)"
-                />
-              </clipPath>
-            </defs>
           </svg>
-        </button>
+          <span className="placeholder">Session Duration:</span>
+          <span className="time">30 minutes per inquiry</span>
+        </p>
+        <a
+          href="https://calendly.com/neetujainca/caneetujain"
+          target="_blank"
+          className="cta-action btn"
+        >
+          Book Your Free Consultation
+          <Image
+            src="/icons/arrow-right.svg"
+            alt="Arrow Right"
+            width={14}
+            height={20}
+          />
+        </a>
         <p className="small">
           *Free consultation is limited to 30min per client and includes general
           advice; detailed financial planning or audit services may incur
